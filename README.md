75021a3afcaa2cdc79de96d23aa884e2a5ab5b7a
David M Sibley <david.m.sibley@gmail.com>
2016-06-07 09:51:53 -0500

    Adding example usage

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # VibrationUtil
 
 Variable duration Android Vibration.  Used by calling as a Unity Engine Plugin.
+
+Nobody should pay money for this dead simple functionality, and nobody should
+spend as much time as I did banging my stupid head on this.
+
+Use [this example](/app/src/test/examples/PluginBridge.cs), put the jar and the
+[example AndroidManifest.xml](/app/src/test/examples/AndroidManifest.xml) in
+the Assets/Plugins/Android folder and be done with it.
---
+++ app/src/test/examples/PluginBridge.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace chillst.plugins {
+	public class PluginBridge {
+		public static void vibrate(long milliseconds) {
+			#if UNITY_ANDROID
+			if (Application.platform == RuntimePlatform.Android) {
+				using (var javaUnityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer")) {
+					using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject> ("currentActivity")) {
+						using (var vibratePlugin = new AndroidJavaObject ("com.chillst.vibrationutil.VibratePlugin")) {
+							vibratePlugin.Call ("vibrate", currentActivity, milliseconds);
+						}
+					}
+				}
+			}
+			#endif
+		}
+	}
+}
